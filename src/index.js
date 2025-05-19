f14fff6260fb63cf38ab82754ae9edb476f3f769
ohnapaula <ohnapaula@gmail.com>
2025-05-19 01:06:44 -0300

    Criando o projeto

---
+++ src/index.js
@@ -0,0 +1,45 @@
+//SEM USAR O EXPRESS
+
+/*const http = require("http");
+
+const hostname = "localhost";   //máscara para o IP
+
+const port = 3333;
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-type", "text/plain");
+    res.end("Hello World!");
+});
+
+server.listen(port, hostname, () =>{
+    console.log("SERVIDOR RODANDO...")
+});*/
+
+const express = require("express"); //Importando um módulo
+const path = require("path");   //Importando um módulo para trabalhar com caminhos
+
+const app = express();  //Trabalhando de fato com o módulo. Puxando um instância do express
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname + "/pages/home.html"));
+
+});
+
+router.get("/contato", (req, res) => {
+
+    res.sendFile(path.join(__dirname + "/pages/contato.html"));
+
+});
+
+app.use(router);
+
+app.listen(3333, () => {
+
+    console.log("Servidor rodando...");
+
+});
+
+
